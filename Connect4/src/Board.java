9bad3a6cffd8037436865d057624d3084d096b3f
StSp18 <andreas.spar@hs-augsburg.de>
2018-03-27 15:05:24 +0200

    made it pretty

--- Connect4/src/Board.java
+++ Connect4/src/Board.java
@@ -1,8 +1,8 @@
 import java.io.IOException;
 
 public class Board {
+	private final int line = 8;			// Zeilenanzahl
 	private final int column = 8;		// Spaltenanzahl
-	private final int line = 10;		// Zeilenanzahl
 	private char[][] board = new char[line][column];
 	private static final char Player1 = 'x';
 	private static final char Player2 = 'o';
@@ -25,29 +25,29 @@ public class Board {
 	// Spielfeld Initialization
 	Board() {
 		// Punkte eintragen
-		for (int i = 1; i < line - 1; i++) {
+		for (int i = 0; i < line; i++) {
 			for (int k = 0; k < column; k++) {
 				board[i][k] = '.';
 			}
-		} 
-		// Spaltenanzahl oben
-		for (int k = 0; k < column; k++) {
-			board[0][k] = (char) ('1' + k);
-		}
-		// Spaltenanzahl unten
-		for (int k = 0; k < column; k++) {
-			board[9][k] = (char) ('1' + k);
 		}
 	}
 
 	public void printBoard() {
 		// Ausgabe des Spielfeld
+		for (int k = 0; k < column; k++) {
+			System.out.print(k+1);
+		}
+		System.out.println();
 		for (int i = 0; i < line; i++) {
 			for (int k = 0; k < column; k++) {
 				System.out.print(board[i][k]);
 			}
 			System.out.println();
 		}
+		for (int k = 0; k < column; k++) {
+			System.out.print(k+1);
+		}
+		System.out.println();
 	}
 
 	public void printWinner() {
@@ -59,7 +59,7 @@ public class Board {
 		// neuen Stein setzten
 		User_Input ui = new User_Input(column);
 		int input = ui.getUserInput();
-		for (int i = line-1; i > 0; i--) {
+		for (int i = line-1; i >= 0; i--) {
 			if (board[i][input] == '.') {
 				board[i][input] = p.getChar();
 				return;
@@ -74,9 +74,9 @@ public class Board {
 		int column;
 		int line;
 		int check;
-		int border = this.line-5 + this.column-4;
+		int border = this.line-3 + this.column-4;
 		// 4 Horizontale gleiche Steine
-		for (int i = 1; i < this.line - 1; i++) {
+		for (int i = 0; i < this.line; i++) {
 			for (int k = 0; k < this.column; k++) {
 //				System.out.print(board[i][k]);
 				if (board[i][k] == p.getChar()) {
@@ -95,7 +95,7 @@ public class Board {
 		buffer = 0;
 		// 4 Vertikale gleiche Steine
 		for (int k = 0; k < this.column; k++) {
-			for (int i = 1; i < this.line - 1; i++) {
+			for (int i = 0; i < this.line; i++) {
 //				System.out.print(board[i][k]);
 				if (board[i][k] == p.getChar()) {
 					buffer++;
@@ -108,14 +108,14 @@ public class Board {
 			buffer = 0;
 //			System.out.println();
 		}
-		line = 4;
+		line = 3;
 		column = 0;
 		// 4 rechts nach links schräg gleiche Steine
 		for(int i=0;i<border;i++) {
 			check = 0;
 			buffer = 0;
-			while(column+check < 8 && line-check > 0) {
-//				System.out.print(board[line-check][column+check]);
+			while(column+check < this.column && line-check >= 0) {
+				System.out.print(board[line-check][column+check]);
 				if(board[line-check][column+check]==p.getChar()) {
 					buffer++;
 					if (buffer == 4) {
@@ -125,20 +125,20 @@ public class Board {
 				
 				check++;
 			}
-//			System.out.println();
-			if(line<8) {
+			System.out.println();
+			if(line<this.line-1) {
 				line++;
 			} else {
 				column++;
 			}
 		}
-		line = 1;
+		line = 0;
 		column = 4;
 		// 4 links nach rechts schräg gleiche Steine
 		for(int i=0;i<border;i++) {
 			buffer = 0;
 			check = 0;
-			while(column+check < 8 && line+check < 9) {
+			while(column+check < this.column && line+check < this.line) {
 //				System.out.print(board[line+check][column+check]);
 				if(board[line+check][column+check]==p.getChar()) {
 					buffer++;
